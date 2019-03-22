9172b9f32e7c57377b1bb100b202a453cb6290d7
Hazizau Kanstantsin <47750585+Gazizoff@users.noreply.github.com>
2019-03-22 18:27:51 +0300

    feat: add function which counts zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+var a = 0;
+var sum = 0;
+var arr = [];
+for ( var i = 2; i <= base; i++){
+    if ((base % i) === 0) {
+        while ((base % i) === 0) {
+            base = base/i;
+            a = a+1;                 
+        }  
+        var t = number;
+        while(t / i >=1) {
+          sum += Math.floor(t/i);
+          t = t/i;
+        }       
+        arr.push(Math.floor(sum/a));
+        a = 0;
+        sum = 0;       
+    }
+    }
+    var min_of_arr = Math.min(...arr);
+    return min_of_arr;
 }
